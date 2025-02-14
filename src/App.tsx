3e8db6bd8223c3561859e1e43beef7393f3718c3
Fatah <alifmiftakhulfatah@gmail.com>
2025-02-14 13:58:57 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+      <div className="bg-[url('/src/assets/bg.png')] min-h-screen bg-cover bg-center bg-no-repeat">
+        <Navbar />
+
+        <section className="container mx-auto px-4 w-full flex h-screen">
+          <div className="flex flex-col justify-center h-full  max-w-[665px]  w-full">
+            <div className="text-center lg:text-start">
+              <h1 className="text-[#1E293B] font-bold text-4xl leading-[50.5px] sm:text-[42px] sm:leading-[57px] lg:text-6xl lg:leading-[76px]">Lessons and insights </h1>
+              <h1 className="text-[#EF4444] font-bold text-4xl leading-[50.5px] sm:text-[42px] sm:leading-[57px] lg:text-6xl lg:leading-[76px]">from 8 years</h1>
+            </div>
+            <div className="text-center lg:text-start py-5">
+              <p className="text-[#1E293B] font-normal text-xs leading-[23px] lg:text-base lg:leading-[26px] xl:text-xl xl:leading-9">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+              </p>
+            </div>
+            <div className="flex justify-center lg:justify-start">
+              <div className="relative w-full max-w-[507px]">
+                <input
+                  type="search"
+                  placeholder="Masukan Kata Sandi"
+                  className="bg-white placeholder-[#9CA3AF] text-sm  w-full text-[#9CA3AF] lg:text-base leading-9 py-5 px-4 rounded-lg outline-none"
+                />
+                <button type="button" className="bg-[#EF4444] text-white font-semibold text-base leading-9 py-2.5 px-6 absolute right-2 top-1/2 transform -translate-y-1/2 rounded-lg">
+                  search
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import menuIcon from "../assets/icon/List.svg";
+import closeIcon from "../assets/icon/close.svg";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const menuNavbar: string[] = ["Home", "Feature", "Service", "Product", "About Us"];
+  return (
+    <>
+      <nav>
+        <div className="container mx-auto flex justify-between lg:justify-end gap-10 items-center p-4">
+          <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <img src={menuIcon} alt="Menu" className="w-8 h-8 transition-transform duration-300 hover:scale-110" />
+          </button>
+
+          <ul className="hidden lg:flex gap-10 text-gray-700">
+            {menuNavbar.map((item) => (
+              <li key={item} className="cursor-pointer text-[#1E293B] font-semibold text-sm">
+                {item}
+              </li>
+            ))}
+            <li className="cursor-pointer text-[#EF4444] font-semibold text-sm">Login</li>
+          </ul>
+
+          <button className="bg-[#EF4444] text-white px-5 py-[10px] rounded-[5px] font-bold text-sm">Sign Up</button>
+        </div>
+
+        <div
+          className={`fixed inset-0 z-50 bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
+          onClick={() => setIsOpen(false)}>
+          <div className={`bg-white w-[250px] h-full p-6 shadow-md transform transition-all duration-500 ease-in-out will-change-transform ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <button className="mb-6 hover:rotate-90 transition-transform duration-300" onClick={() => setIsOpen(false)}>
+              <img src={closeIcon} alt="Close" className="w-[15px] h-[21px]" />
+            </button>
+
+            <ul className="space-y-10">
+              {menuNavbar.map((item) => (
+                <li key={item} className="text-[#1E293B] font-bold text-sm cursor-pointer">
+                  {item}
+                </li>
+              ))}
+              <li className="cursor-pointer text-[#EF4444] font-bold text-sm">Login</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
